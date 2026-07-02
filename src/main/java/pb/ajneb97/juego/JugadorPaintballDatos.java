9a099f73957c5a1997c7cb9ecb76e6d20b42b573
Nicouschulas <156347186+Nicouschulas@users.noreply.github.com>
2026-07-02 22:34:23 +0200

    refactor(JugadorPaintballDatos): Suppress unused warnings for data model getters and setters

--- src/main/java/pb/ajneb97/juego/JugadorPaintballDatos.java
+++ src/main/java/pb/ajneb97/juego/JugadorPaintballDatos.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import pb.ajneb97.api.Hat;
 import pb.ajneb97.api.Perk;
 
+@SuppressWarnings("unused")
 public class JugadorPaintballDatos {
 
 	private int wins;
@@ -13,6 +14,7 @@ public class JugadorPaintballDatos {
 	private int kills;
 	private ArrayList<Hat> hats;
 	private ArrayList<Perk> perks;
+
 	public JugadorPaintballDatos(int wins, int loses, int ties, int kills, ArrayList<Hat> hats, ArrayList<Perk> perks) {
 		this.wins = wins;
 		this.loses = loses;
@@ -21,42 +23,52 @@ public class JugadorPaintballDatos {
 		this.hats = hats;
 		this.perks = perks;
 	}
+
 	public int getWins() {
 		return wins;
 	}
+
 	public int getKills() {
 		return kills;
 	}
+
 	public void setKills(int kills) {
 		this.kills = kills;
 	}
+
 	public void setWins(int wins) {
 		this.wins = wins;
 	}
+
 	public int getLoses() {
 		return loses;
 	}
+
 	public void setLoses(int loses) {
 		this.loses = loses;
 	}
+
 	public int getTies() {
 		return ties;
 	}
+
 	public void setTies(int ties) {
 		this.ties = ties;
 	}
+
 	public ArrayList<Hat> getHats() {
 		return hats;
 	}
+
 	public void setHats(ArrayList<Hat> hats) {
 		this.hats = hats;
 	}
+
 	public ArrayList<Perk> getPerks() {
 		return perks;
 	}
+
 	public void setPerks(ArrayList<Perk> perks) {
 		this.perks = perks;
 	}
-	
-	
 }
