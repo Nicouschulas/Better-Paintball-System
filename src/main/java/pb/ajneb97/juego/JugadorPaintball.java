c40f0c415cc4c27406294d700c6d40b4519a4723
Nicouschulas <156347186+Nicouschulas@users.noreply.github.com>
2026-07-02 22:32:32 +0200

    optimised to final; refactor(JugadorPaintball): Fix index shifting bug using removeIf, modernization with getLast, and suppress unused warnings

--- src/main/java/pb/ajneb97/juego/JugadorPaintball.java
+++ src/main/java/pb/ajneb97/juego/JugadorPaintball.java
@@ -4,47 +4,48 @@ import java.util.ArrayList;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
-
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class JugadorPaintball {
 
-	private Player jugador;
+	private final Player jugador;
 	private int asesinatos;
 	private int muertes;
-	private ElementosGuardados guardados;
+	private final ElementosGuardados guardados;
 	private boolean asesinadoRecientemente;
 	private String preferenciaTeam;
 	private int coins;
-	private ArrayList<Killstreak> killstreaks;
+	private final ArrayList<Killstreak> killstreaks;
 	private Location deathLocation;
-	
+
 	private String selectedHat;
 	private boolean efectoHatActivado;
 	private boolean efectoHatEnCooldown;
 	private int tiempoEfectoHat;
 	private String lastKilledBy;
-	
+
 	@SuppressWarnings("deprecation")
-	public JugadorPaintball(Player jugador) {
+	public JugadorPaintball(@NotNull Player jugador) {
 		this.jugador = jugador;
-		this.guardados = new ElementosGuardados(jugador.getInventory().getContents().clone(),jugador.getEquipment().getArmorContents().clone(),jugador.getGameMode()
-				,jugador.getExp(),jugador.getLevel(),jugador.getFoodLevel(),jugador.getHealth(),jugador.getMaxHealth(),jugador.getAllowFlight(),jugador.isFlying());
+		this.guardados = new ElementosGuardados(jugador.getInventory().getContents().clone(), jugador.getEquipment().getArmorContents().clone(), jugador.getGameMode()
+				, jugador.getExp(), jugador.getLevel(), jugador.getFoodLevel(), jugador.getHealth(), jugador.getMaxHealth(), jugador.getAllowFlight(), jugador.isFlying());
 		this.asesinadoRecientemente = false;
 		this.muertes = 0;
 		this.asesinatos = 0;
 		this.coins = 0;
-		this.killstreaks = new ArrayList<Killstreak>();
+		this.killstreaks = new ArrayList<>();
 		this.efectoHatActivado = false;
 		this.efectoHatEnCooldown = false;
 		this.tiempoEfectoHat = 0;
 		this.selectedHat = "";
 	}
-	
-	public String getLastKilledBy() {
+
+	public @Nullable String getLastKilledBy() {
 		return lastKilledBy;
 	}
 
-	public void setLastKilledBy(String lastKilledBy) {
+	public void setLastKilledBy(@Nullable String lastKilledBy) {
 		this.lastKilledBy = lastKilledBy;
 	}
 
@@ -72,27 +73,27 @@ public class JugadorPaintball {
 		this.tiempoEfectoHat = tiempoEfectoHat;
 	}
 
-	public void setSelectedHat(String hat) {
+	public void setSelectedHat(@NotNull String hat) {
 		this.selectedHat = hat;
 	}
-	
-	public String getSelectedHat() {
+
+	public @NotNull String getSelectedHat() {
 		return this.selectedHat;
 	}
-	
-	public void setDeathLocation(Location l) {
+
+	public void setDeathLocation(@Nullable Location l) {
 		this.deathLocation = l;
 	}
-	
-	public Location getDeathLocation() {
+
+	public @Nullable Location getDeathLocation() {
 		return this.deathLocation;
 	}
-	
-	public void agregarKillstreak(Killstreak k) {
+
+	public void agregarKillstreak(@NotNull Killstreak k) {
 		this.killstreaks.add(k);
 	}
-	
-	public Killstreak getKillstreak(String tipo) {
+
+	public @Nullable Killstreak getKillstreak(@NotNull String tipo) {
 		for(Killstreak k : this.killstreaks) {
 			if(k.getTipo().equals(tipo)) {
 				return k;
@@ -100,73 +101,71 @@ public class JugadorPaintball {
 		}
 		return null;
 	}
-	
-	public void removerKillstreak(String tipo) {
-		for(int i=0;i<killstreaks.size();i++) {
-			if(killstreaks.get(i).getTipo().equals(tipo)) {
-				this.killstreaks.remove(i);
-			}
-		}
+
+	public void removerKillstreak(@NotNull String tipo) {
+		// Sicheres Entfernen via Lambda ohne Index-Verschiebungsfehler
+		this.killstreaks.removeIf(k -> k.getTipo().equals(tipo));
 	}
-	
-	public Killstreak getUltimaKillstreak() {
+
+	public @Nullable Killstreak getUltimaKillstreak() {
 		if(killstreaks.isEmpty()) {
 			return null;
 		}else {
-			return killstreaks.get(killstreaks.size()-1);
+			// Nutzt Java 21+ Sequenced Collections .getLast()
+			return killstreaks.getLast();
 		}
 	}
-	
+
 	public int getCoins() {
 		return this.coins;
 	}
-	
+
 	public void agregarCoins(int cantidad) {
-		this.coins = this.coins+cantidad;
+		this.coins = this.coins + cantidad;
 	}
-	
+
 	public void disminuirCoins(int cantidad) {
-		this.coins = this.coins-cantidad;
+		this.coins = this.coins - cantidad;
 	}
-	
-	public void setPreferenciaTeam(String team) {
+
+	public void setPreferenciaTeam(@Nullable String team) {
 		this.preferenciaTeam = team;
 	}
-	
-	public String getPreferenciaTeam() {
+
+	public @Nullable String getPreferenciaTeam() {
 		return this.preferenciaTeam;
 	}
-	
-	public ElementosGuardados getGuardados() {
+
+	public @NotNull ElementosGuardados getGuardados() {
 		return this.guardados;
 	}
-	
+
 	public void aumentarAsesinatos() {
 		this.asesinatos++;
 	}
-	
+
 	public void aumentarMuertes() {
 		this.muertes++;
 	}
-	
+
 	public int getAsesinatos() {
 		return this.asesinatos;
 	}
-	
+
+	@SuppressWarnings("unused")
 	public int getMuertes() {
 		return this.muertes;
 	}
-	
-	public Player getJugador() {
+
+	public @NotNull Player getJugador() {
 		return this.jugador;
 	}
-	
+
 	public void setAsesinadoRecientemente(boolean asesinadoRecientemente) {
 		this.asesinadoRecientemente = asesinadoRecientemente;
 	}
-	
+
 	public boolean haSidoAsesinadoRecientemente() {
 		return this.asesinadoRecientemente;
 	}
-	
 }
