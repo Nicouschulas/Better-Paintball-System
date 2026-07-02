955507cc08abcc61b3567847a6d467606fd9f36f
Nicouschulas <156347186+Nicouschulas@users.noreply.github.com>
2026-07-02 22:57:45 +0200

    optimising some stuff

--- src/main/java/pb/ajneb97/managers/Actualizacion.java
+++ src/main/java/pb/ajneb97/managers/Actualizacion.java
@@ -6,26 +6,29 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-
+import org.jetbrains.annotations.NotNull;
 import pb.ajneb97.PaintballBattle;
 
-public class Actualizacion implements Listener{
+public class Actualizacion implements Listener {
 
-	private PaintballBattle plugin;
-	public Actualizacion(PaintballBattle plugin){		
-		this.plugin = plugin;		
+	private final PaintballBattle plugin;
+
+	public Actualizacion(@NotNull PaintballBattle plugin) {
+		this.plugin = plugin;
 	}
-	
+
 	@EventHandler
-	public void Join(PlayerJoinEvent event){
+	public void Join(@NotNull PlayerJoinEvent event) {
 		Player jugador = event.getPlayer();
-		FileConfiguration config = plugin.getConfig();		
-		if(jugador.isOp() && !(plugin.version.equals(plugin.latestversion))){
-			if(config.getString("new_version_reminder").equals("true")){
-				jugador.sendMessage(PaintballBattle.prefix + ChatColor.RED +" There is a new version available. "+ChatColor.YELLOW+
-		  				  "("+ChatColor.GRAY+plugin.latestversion+ChatColor.YELLOW+")");
-		  		    jugador.sendMessage(ChatColor.RED+"You can download it at: "+ChatColor.GREEN+"https://www.spigotmc.org/resources/76676/");
-			}			 
+
+		if (jugador.isOp() && !plugin.version.equals(plugin.latestversion)) {
+			FileConfiguration config = plugin.getConfig();
+
+			if (config.getBoolean("new_version_reminder", true)) {
+				jugador.sendMessage(PaintballBattle.prefix + ChatColor.RED + " There is a new version available. " + ChatColor.YELLOW +
+						"(" + ChatColor.GRAY + plugin.latestversion + ChatColor.YELLOW + ")");
+				jugador.sendMessage(ChatColor.RED + "You can download it at: " + ChatColor.GREEN + "https://www.spigotmc.org/resources/76676/");
+			}
 		}
 	}
 }
