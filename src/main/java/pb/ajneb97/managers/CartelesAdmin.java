dd01baa18eb88c40ac61b10679a1d511cc53f6b1
Nicouschulas <156347186+Nicouschulas@users.noreply.github.com>
2026-07-02 22:58:40 +0200

    improvments

--- src/main/java/pb/ajneb97/managers/CartelesAdmin.java
+++ src/main/java/pb/ajneb97/managers/CartelesAdmin.java
@@ -18,7 +18,7 @@ import pb.ajneb97.juego.Partida;
 public class CartelesAdmin {
 	
 	private int taskID;
-	private PaintballBattle plugin;
+	private final PaintballBattle plugin;
 	public CartelesAdmin(PaintballBattle plugin){		
 		this.plugin = plugin;		
 	}
@@ -43,49 +43,49 @@ public class CartelesAdmin {
 			for(String arena : config.getConfigurationSection("Signs").getKeys(false)) {
 				Partida partida = plugin.getPartida(arena);
 				if(partida != null) {
-					List<String> listaCarteles = new ArrayList<String>();
+					List<String> listaCarteles = new ArrayList<>();
 					if(config.contains("Signs."+arena)) {
 						listaCarteles = config.getStringList("Signs."+arena);
 					}
-					for(int i=0;i<listaCarteles.size();i++) {
-						String[] separados = listaCarteles.get(i).split(";");
-						int x = Integer.valueOf(separados[0]);
-						int y = Integer.valueOf(separados[1]);
-						int z = Integer.valueOf(separados[2]);
-						World world = Bukkit.getWorld(separados[3]);
-						if(world != null) {
-							int chunkX = x >> 4;
-							int chunkZ = z >> 4;
-							if(!world.isChunkLoaded(chunkX, chunkZ)) {
-								continue;
-							}
-							Block block = world.getBlockAt(x,y,z);
-							
-							if(block.getType().name().contains("SIGN")) {
-								Sign sign = (Sign) block.getState();
-								String estado = "";
-								if(partida.getEstado().equals(EstadoPartida.JUGANDO)) {
-									estado = messages.getString("signStatusIngame");
-								}else if(partida.getEstado().equals(EstadoPartida.COMENZANDO)) {
-									estado = messages.getString("signStatusStarting");
-								}else if(partida.getEstado().equals(EstadoPartida.ESPERANDO)) {
-									estado = messages.getString("signStatusWaiting");
-								}else if(partida.getEstado().equals(EstadoPartida.DESACTIVADA)) {
-									estado = messages.getString("signStatusDisabled");
-								}else if(partida.getEstado().equals(EstadoPartida.TERMINANDO)) {
-									estado = messages.getString("signStatusFinishing");
-								}
-								
-								List<String> lista = messages.getStringList("signFormat");
-								for(int c=0;c<lista.size();c++) {
-									sign.setLine(c, ChatColor.translateAlternateColorCodes('&', lista.get(c).replace("%arena%", arena).replace("%current_players%", partida.getCantidadActualJugadores()+"")
-											.replace("%max_players%", partida.getCantidadMaximaJugadores()+"").replace("%status%", estado)));
-								}
+                    for (String listaCartele : listaCarteles) {
+                        String[] separados = listaCartele.split(";");
+                        int x = Integer.valueOf(separados[0]);
+                        int y = Integer.valueOf(separados[1]);
+                        int z = Integer.valueOf(separados[2]);
+                        World world = Bukkit.getWorld(separados[3]);
+                        if (world != null) {
+                            int chunkX = x >> 4;
+                            int chunkZ = z >> 4;
+                            if (!world.isChunkLoaded(chunkX, chunkZ)) {
+                                continue;
+                            }
+                            Block block = world.getBlockAt(x, y, z);
 
-								sign.update();
-							}
-						}
-					}
+                            if (block.getType().name().contains("SIGN")) {
+                                Sign sign = (Sign) block.getState();
+                                String estado = "";
+                                if (partida.getEstado().equals(EstadoPartida.JUGANDO)) {
+                                    estado = messages.getString("signStatusIngame");
+                                } else if (partida.getEstado().equals(EstadoPartida.COMENZANDO)) {
+                                    estado = messages.getString("signStatusStarting");
+                                } else if (partida.getEstado().equals(EstadoPartida.ESPERANDO)) {
+                                    estado = messages.getString("signStatusWaiting");
+                                } else if (partida.getEstado().equals(EstadoPartida.DESACTIVADA)) {
+                                    estado = messages.getString("signStatusDisabled");
+                                } else if (partida.getEstado().equals(EstadoPartida.TERMINANDO)) {
+                                    estado = messages.getString("signStatusFinishing");
+                                }
+
+                                List<String> lista = messages.getStringList("signFormat");
+                                for (int c = 0; c < lista.size(); c++) {
+                                    sign.setLine(c, ChatColor.translateAlternateColorCodes('&', lista.get(c).replace("%arena%", arena).replace("%current_players%", partida.getCantidadActualJugadores() + "")
+                                            .replace("%max_players%", partida.getCantidadMaximaJugadores() + "").replace("%status%", estado)));
+                                }
+
+                                sign.update();
+                            }
+                        }
+                    }
 					
 				}
 			}
