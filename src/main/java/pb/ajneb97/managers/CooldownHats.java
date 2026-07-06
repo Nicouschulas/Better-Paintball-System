4ffae2385c1586d8b814fa766c9b55f85efc958b
Nicouschulas <156347186+Nicouschulas@users.noreply.github.com>
2026-07-06 15:01:27 +0200

    refactor(CooldownHats): Simplify task loops using lambdas and remove redundant returns and final

--- src/main/java/pb/ajneb97/managers/CooldownHats.java
+++ src/main/java/pb/ajneb97/managers/CooldownHats.java
@@ -1,10 +1,10 @@
 package pb.ajneb97.managers;
 
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.scheduler.BukkitScheduler;
+import org.jetbrains.annotations.NotNull;
 
 import pb.ajneb97.PaintballBattle;
 import pb.ajneb97.juego.EstadoPartida;
@@ -17,29 +17,28 @@ public class CooldownHats {
 	int tiempo;
 	private JugadorPaintball jugador;
 	private Partida partida;
-	private PaintballBattle plugin;
-	public CooldownHats(PaintballBattle plugin){		
-		this.plugin = plugin;		
+	private final PaintballBattle plugin;
+
+	public CooldownHats(@NotNull PaintballBattle plugin){
+		this.plugin = plugin;
 	}
-	
-	public void cooldownHat(final JugadorPaintball jugador, final Partida partida,int tiempo){
+
+	public void cooldownHat(final JugadorPaintball jugador, final Partida partida, int tiempo){
 		this.jugador = jugador;
 		this.tiempo = tiempo;
 		this.partida = partida;
 		BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
- 	    taskID = scheduler.scheduleSyncRepeatingTask(plugin, new Runnable() {
-		public void run(){
+
+		taskID = scheduler.scheduleSyncRepeatingTask(plugin, () -> {
 			if(!ejecutarCooldownHat()){
 				FileConfiguration messages = plugin.getMessages();
 				if(!partida.getEstado().equals(EstadoPartida.TERMINANDO)) {
 					jugador.getJugador().sendMessage(ChatColor.translateAlternateColorCodes('&', messages.getString("hatCooldownFinished")));
 				}
-				
+
 				Bukkit.getScheduler().cancelTask(taskID);
-				return;
-			}	
- 		   }
- 	   }, 0L, 20L);
+			}
+		}, 0L, 20L);
 	}
 
 	protected boolean ejecutarCooldownHat() {
@@ -57,22 +56,20 @@ public class CooldownHats {
 			return false;
 		}
 	}
-	
-	public void durationHat(final JugadorPaintball jugador, final Partida partida,int tiempo){
+
+	public void durationHat(final JugadorPaintball jugador, final Partida partida, int tiempo){
 		this.jugador = jugador;
 		this.tiempo = tiempo;
 		this.partida = partida;
 		BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
- 	    taskID = scheduler.scheduleSyncRepeatingTask(plugin, new Runnable() {
-		public void run(){
+
+		taskID = scheduler.scheduleSyncRepeatingTask(plugin, () -> {
 			if(!ejecutarDurationHat()){
 				Bukkit.getScheduler().cancelTask(taskID);
-				return;
-			}	
- 		   }
- 	   }, 0L, 20L);
+			}
+		}, 0L, 20L);
 	}
-	
+
 	protected boolean ejecutarDurationHat() {
 		if(partida != null && partida.getEstado().equals(EstadoPartida.JUGANDO)) {
 			if(tiempo <= 0) {
