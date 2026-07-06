a962cc1a2ac5499d3fc5763740640bb5112e5fb6
Nicouschulas <156347186+Nicouschulas@users.noreply.github.com>
2026-07-06 14:38:56 +0200

    refactor(CartelesAdmin): Optimize task scheduling to method reference, fix redundant boxing via parseInt, and secure sign status lookups

--- src/main/java/pb/ajneb97/managers/CartelesAdmin.java
+++ src/main/java/pb/ajneb97/managers/CartelesAdmin.java
@@ -8,51 +8,55 @@ import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.scheduler.BukkitScheduler;
+import org.jetbrains.annotations.NotNull;
 
 import pb.ajneb97.PaintballBattle;
 import pb.ajneb97.juego.EstadoPartida;
 import pb.ajneb97.juego.Partida;
 
 public class CartelesAdmin {
-	
-	private int taskID;
-	private final PaintballBattle plugin;
-	public CartelesAdmin(PaintballBattle plugin){		
-		this.plugin = plugin;		
-	}
-	
-	public int getTaskID() {
-		return this.taskID;
-	}
-	
-	public void actualizarCarteles() {
-	    BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
- 	    taskID = scheduler.scheduleSyncRepeatingTask(plugin, new Runnable() {
-            public void run() { 
-            	ejecutarActualizarCarteles();
-            }
-        },0, 30L);
-	}
 
-	protected void ejecutarActualizarCarteles() {
-		FileConfiguration config = plugin.getConfig();
-		FileConfiguration messages = plugin.getMessages();
-		if(config.contains("Signs")) {
-			for(String arena : config.getConfigurationSection("Signs").getKeys(false)) {
-				Partida partida = plugin.getPartida(arena);
-				if(partida != null) {
-					List<String> listaCarteles = new ArrayList<>();
-					if(config.contains("Signs."+arena)) {
-						listaCarteles = config.getStringList("Signs."+arena);
-					}
+    private int taskID;
+    private final PaintballBattle plugin;
+
+    public CartelesAdmin(@NotNull PaintballBattle plugin){
+        this.plugin = plugin;
+    }
+
+    public int getTaskID() {
+        return this.taskID;
+    }
+
+    public void actualizarCarteles() {
+        BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+        taskID = scheduler.scheduleSyncRepeatingTask(plugin, this::ejecutarActualizarCarteles, 0, 30L);
+    }
+
+    protected void ejecutarActualizarCarteles() {
+        FileConfiguration config = plugin.getConfig();
+        FileConfiguration messages = plugin.getMessages();
+
+        ConfigurationSection signsSection = config.getConfigurationSection("Signs");
+        if (signsSection != null) {
+            for (String arena : signsSection.getKeys(false)) {
+                Partida partida = plugin.getPartida(arena);
+                if (partida != null) {
+                    List<String> listaCarteles = new ArrayList<>();
+                    if (config.contains("Signs." + arena)) {
+                        listaCarteles = config.getStringList("Signs." + arena);
+                    }
                     for (String listaCartele : listaCarteles) {
                         String[] separados = listaCartele.split(";");
-                        int x = Integer.valueOf(separados[0]);
-                        int y = Integer.valueOf(separados[1]);
-                        int z = Integer.valueOf(separados[2]);
+                        if (separados.length < 4) continue;
+
+                        int x = Integer.parseInt(separados[0]);
+                        int y = Integer.parseInt(separados[1]);
+                        int z = Integer.parseInt(separados[2]);
                         World world = Bukkit.getWorld(separados[3]);
+
                         if (world != null) {
                             int chunkX = x >> 4;
                             int chunkZ = z >> 4;
@@ -63,33 +67,37 @@ public class CartelesAdmin {
 
                             if (block.getType().name().contains("SIGN")) {
                                 Sign sign = (Sign) block.getState();
-                                String estado = "";
+
+                                String estado = "UNKNOWN";
                                 if (partida.getEstado().equals(EstadoPartida.JUGANDO)) {
-                                    estado = messages.getString("signStatusIngame");
+                                    estado = messages.getString("signStatusIngame", "Ingame");
                                 } else if (partida.getEstado().equals(EstadoPartida.COMENZANDO)) {
-                                    estado = messages.getString("signStatusStarting");
+                                    estado = messages.getString("signStatusStarting", "Starting");
                                 } else if (partida.getEstado().equals(EstadoPartida.ESPERANDO)) {
-                                    estado = messages.getString("signStatusWaiting");
+                                    estado = messages.getString("signStatusWaiting", "Waiting");
                                 } else if (partida.getEstado().equals(EstadoPartida.DESACTIVADA)) {
-                                    estado = messages.getString("signStatusDisabled");
+                                    estado = messages.getString("signStatusDisabled", "Disabled");
                                 } else if (partida.getEstado().equals(EstadoPartida.TERMINANDO)) {
-                                    estado = messages.getString("signStatusFinishing");
+                                    estado = messages.getString("signStatusFinishing", "Finishing");
                                 }
 
                                 List<String> lista = messages.getStringList("signFormat");
                                 for (int c = 0; c < lista.size(); c++) {
-                                    sign.setLine(c, ChatColor.translateAlternateColorCodes('&', lista.get(c).replace("%arena%", arena).replace("%current_players%", partida.getCantidadActualJugadores() + "")
-                                            .replace("%max_players%", partida.getCantidadMaximaJugadores() + "").replace("%status%", estado)));
+                                    String lineText = lista.get(c)
+                                            .replace("%arena%", arena)
+                                            .replace("%current_players%", String.valueOf(partida.getCantidadActualJugadores()))
+                                            .replace("%max_players%", String.valueOf(partida.getCantidadMaximaJugadores()))
+                                            .replace("%status%", estado);
+
+                                    sign.setLine(c, ChatColor.translateAlternateColorCodes('&', lineText));
                                 }
 
                                 sign.update();
                             }
                         }
                     }
-					
-				}
-			}
-		}
-		
-	}
+                }
+            }
+        }
+    }
 }
