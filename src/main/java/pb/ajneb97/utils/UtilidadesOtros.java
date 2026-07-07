96dbaf5e1bfe1a9670d595c7b4e489f4e0e32e6f
Nicouschulas <156347186+Nicouschulas@users.noreply.github.com>
2026-07-07 11:02:06 +0200

    refactor(UtilidadesOtros): Resolve static analysis warnings, fix configuration NPEs, improve enum comparison fix redundant branches and handle empty reflection catch blocks

--- src/main/java/pb/ajneb97/utils/UtilidadesOtros.java
+++ src/main/java/pb/ajneb97/utils/UtilidadesOtros.java
@@ -21,68 +21,58 @@ import org.bukkit.inventory.PlayerInventory;
 public class UtilidadesOtros {
 
 	public static boolean isChatNew() {
-        ServerVersion serverVersion = PaintballBattle.serverVersion;
-        if(serverVersion.serverVersionGreaterEqualThan(serverVersion,ServerVersion.v1_19_R1)){
-            return true;
-        }
-        return false;
-    }
+		ServerVersion serverVersion = PaintballBattle.serverVersion;
+		return serverVersion.serverVersionGreaterEqualThan(serverVersion, ServerVersion.v1_19_R1);
+	}
 
-    public static boolean isNew() {
-        ServerVersion serverVersion = PaintballBattle.serverVersion;
-        if(serverVersion.serverVersionGreaterEqualThan(serverVersion,ServerVersion.v1_16_R1)){
-            return true;
-        }
-        return false;
-    }
+	public static boolean isNew() {
+		ServerVersion serverVersion = PaintballBattle.serverVersion;
+		return serverVersion.serverVersionGreaterEqualThan(serverVersion, ServerVersion.v1_16_R1);
+	}
 
-    public static boolean isLegacy() {
-        ServerVersion serverVersion = PaintballBattle.serverVersion;
-        if(serverVersion.serverVersionGreaterEqualThan(serverVersion,ServerVersion.v1_13_R1)){
-            return false;
-        }else {
-            return true;
-        }
-    }
-    
+	public static boolean isLegacy() {
+		ServerVersion serverVersion = PaintballBattle.serverVersion;
+		return !serverVersion.serverVersionGreaterEqualThan(serverVersion, ServerVersion.v1_13_R1);
+	}
+
 	public static String getTiempo(int tiempo) {
 		int minutos = tiempo/60;
 		int segundos = tiempo - (minutos*60);
-		String segundosMsg = "";
-		String minutosMsg = "";
+		String segundosMsg;
+		String minutosMsg;
 		if(segundos >= 0 && segundos <= 9) {
 			segundosMsg = "0"+segundos;
 		}else {
 			segundosMsg = segundos+"";
 		}
-		
+
 		if(minutos >= 0 && minutos <= 9) {
 			minutosMsg = "0"+minutos;
 		}else {
 			minutosMsg = minutos+"";
 		}
-		
+
 		return minutosMsg+":"+segundosMsg;
-	}	
-	
+	}
+
 	public static int coinsGanados(Player jugador,FileConfiguration config) {
 		String coinsString = config.getString("coins_per_kill");
-		if(coinsString.contains("-")) {
+		if(coinsString != null && coinsString.contains("-")) {
 			String[] separados = coinsString.split("-");
-			int num1 = Integer.valueOf(separados[0]);
-			int num2 = Integer.valueOf(separados[1]);
+			int num1 = Integer.parseInt(separados[0]);
+			int num2 = Integer.parseInt(separados[1]);
 			return getNumeroAleatorio(num1,num2);
-		}else {
-			return Integer.valueOf(coinsString);
+		}else if(coinsString != null) {
+			return Integer.parseInt(coinsString);
 		}
+		return 0;
 	}
-	
+
 	public static int getNumeroAleatorio(int min, int max) {
 		Random r = new Random();
-		int numero = r.nextInt((max - min) + 1) + min;
-		return numero;
+		return r.nextInt((max - min) + 1) + min;
 	}
-	
+
 	public static void generarParticula(String particle, Location l, float xOffset, float yOffset, float zOffset, float speed, int count) {
 		if(UtilidadesOtros.isLegacy()) {
 			float red = 0;
@@ -92,7 +82,7 @@ public class UtilidadesOtros {
 			if(particle.startsWith("REDSTONE;")) {
 				redstone = true;
 				String[] sep = particle.split(";");
-				int rgb = Integer.valueOf(sep[1]);
+				int rgb = Integer.parseInt(sep[1]);
 				particle = sep[0];
 				Color color = Color.fromRGB(rgb);
 				red = (float) color.getRed()/255;
@@ -100,151 +90,144 @@ public class UtilidadesOtros {
 				blue = (float) color.getBlue()/255;
 			}
 			try {
-				//Revisar el particle de REDSTONE
 				Class<?> packetEnumParticle = getNMSClass("EnumParticle");
 				Method packetEnumMethod = packetEnumParticle.getMethod("valueOf", String.class);
 				Object enumParticle = packetEnumMethod.invoke(null,particle);
 				Class<?> packetClass = getNMSClass("PacketPlayOutWorldParticles");
-				
+
 				Constructor<?> packetConstructor = null;
 				for(Constructor<?> c : packetClass.getConstructors()) {
 					if(c.toGenericString().contains("EnumParticle")) {
 						packetConstructor = c;
 					}
 				}
-//				Constructor<?> packetConstructor = packetClass.getConstructor(packetEnumParticle, boolean.class, float.class, float.class, float.class, float.class, float.class, float.class,
-//						float.class, int.class, int.class);
-				Object packet = null;
-				if(redstone) {
-					packet = packetConstructor.newInstance(enumParticle, true, (float)l.getX(), (float)l.getY(), (float)l.getZ(), red, green, blue, count, 0, null);
-				}else {
-					packet = packetConstructor.newInstance(enumParticle, false, (float)l.getX(), (float)l.getY(), (float)l.getZ(), (float)xOffset, (float)yOffset, (float)yOffset, speed, count, null);
+
+				if (packetConstructor != null) {
+					Object packet;
+					if(redstone) {
+						packet = packetConstructor.newInstance(enumParticle, true, (float)l.getX(), (float)l.getY(), (float)l.getZ(), red, green, blue, count, 0, null);
+					}else {
+						packet = packetConstructor.newInstance(enumParticle, false, (float)l.getX(), (float)l.getY(), (float)l.getZ(), xOffset, yOffset, yOffset, speed, count, null);
+					}
+					Method sendPacket = getNMSClass("PlayerConnection").getMethod("sendPacket", getNMSClass("Packet"));
+					for(Player player : Bukkit.getOnlinePlayers()) {
+						sendPacket.invoke(getConnection(player), packet);
+					}
 				}
-		        Method sendPacket = getNMSClass("PlayerConnection").getMethod("sendPacket", getNMSClass("Packet"));
-		        for(Player player : Bukkit.getOnlinePlayers()) {
-		        	sendPacket.invoke(getConnection(player), packet);
-		        }
-		        
+
 			} catch (ClassNotFoundException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
-					| SecurityException | NoSuchMethodException | NoSuchFieldException | InstantiationException e) {
-				
+			         | SecurityException | NoSuchMethodException | NoSuchFieldException | InstantiationException _) {
 			}
 		}else {
-			l.getWorld().spawnParticle(Particle.valueOf(particle),l,count,xOffset,yOffset,zOffset,speed);
+			if (l.getWorld() != null) {
+				l.getWorld().spawnParticle(Particle.valueOf(particle),l,count,xOffset,yOffset,zOffset,speed);
+			}
 		}
 	}
-	
+
 	private static Class<?> getNMSClass(String nmsClassString) throws ClassNotFoundException {
-        String version = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3] + ".";
-        String name = "net.minecraft.server." + version + nmsClassString;
-        Class<?> nmsClass = Class.forName(name);
-        return nmsClass;
-    }
-	
-    private static Object getConnection(Player player) throws SecurityException, NoSuchMethodException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException, InvocationTargetException {
-        Method getHandle = player.getClass().getMethod("getHandle");
-        Object nmsPlayer = getHandle.invoke(player);
-        Field conField = nmsPlayer.getClass().getField("playerConnection");
-        Object con = conField.get(nmsPlayer);
-        return con;
-    }
-	
+		String version = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3] + ".";
+		String name = "net.minecraft.server." + version + nmsClassString;
+		return Class.forName(name);
+	}
+
+	private static Object getConnection(Player player) throws SecurityException, NoSuchMethodException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+		Method getHandle = player.getClass().getMethod("getHandle");
+		Object nmsPlayer = getHandle.invoke(player);
+		Field conField = nmsPlayer.getClass().getField("playerConnection");
+		return conField.get(nmsPlayer);
+	}
+
 	public static boolean pasaConfigInventario(Player jugador,FileConfiguration config) {
-		if(config.getString("empty_inventory_to_join").equals("true")) {
+		if("true".equals(config.getString("empty_inventory_to_join"))) {
 			PlayerInventory inv = jugador.getInventory();
 			for(ItemStack item : inv.getContents()) {
-				if(item != null && !item.getType().equals(Material.AIR)) {
+				if(item != null && item.getType() != Material.AIR) {
 					return false;
 				}
 			}
 			for(ItemStack item : inv.getArmorContents()) {
-				if(item != null && !item.getType().equals(Material.AIR)) {
+				if(item != null && item.getType() != Material.AIR) {
 					return false;
 				}
 			}
-			return true;
-		}else {
-			return true;
 		}
+		return true;
 	}
-	
+
 	public static double eval(final String str) {
-	    return new Object() {
-	        int pos = -1, ch;
+		return new Object() {
+			int pos = -1, ch;
 
-	        void nextChar() {
-	            ch = (++pos < str.length()) ? str.charAt(pos) : -1;
-	        }
+			void nextChar() {
+				ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+			}
 
-	        boolean eat(int charToEat) {
-	            while (ch == ' ') nextChar();
-	            if (ch == charToEat) {
-	                nextChar();
-	                return true;
-	            }
-	            return false;
-	        }
+			boolean eat(int charToEat) {
+				while (ch == ' ') nextChar();
+				if (ch == charToEat) {
+					nextChar();
+					return true;
+				}
+				return false;
+			}
 
-	        double parse() {
-	            nextChar();
-	            double x = parseExpression();
-	            if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char)ch);
-	            return x;
-	        }
+			double parse() {
+				nextChar();
+				double x = parseExpression();
+				if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char)ch);
+				return x;
+			}
 
-	        // Grammar:
-	        // expression = term | expression `+` term | expression `-` term
-	        // term = factor | term `*` factor | term `/` factor
-	        // factor = `+` factor | `-` factor | `(` expression `)`
-	        //        | number | functionName factor | factor `^` factor
+			double parseExpression() {
+				double x = parseTerm();
+				for (;;) {
+					if      (eat('+')) x += parseTerm(); // addition
+					else if (eat('-')) x -= parseTerm(); // subtraction
+					else return x;
+				}
+			}
 
-	        double parseExpression() {
-	            double x = parseTerm();
-	            for (;;) {
-	                if      (eat('+')) x += parseTerm(); // addition
-	                else if (eat('-')) x -= parseTerm(); // subtraction
-	                else return x;
-	            }
-	        }
+			double parseTerm() {
+				double x = parseFactor();
+				for (;;) {
+					if      (eat('*')) x *= parseFactor(); // multiplication
+					else if (eat('/')) x /= parseFactor(); // division
+					else return x;
+				}
+			}
 
-	        double parseTerm() {
-	            double x = parseFactor();
-	            for (;;) {
-	                if      (eat('*')) x *= parseFactor(); // multiplication
-	                else if (eat('/')) x /= parseFactor(); // division
-	                else return x;
-	            }
-	        }
+			double parseFactor() {
+				if (eat('+')) return parseFactor(); // unary plus
+				if (eat('-')) return -parseFactor(); // unary minus
 
-	        double parseFactor() {
-	            if (eat('+')) return parseFactor(); // unary plus
-	            if (eat('-')) return -parseFactor(); // unary minus
+				double x;
+				int startPos = this.pos;
+				if (eat('(')) { // parentheses
+					x = parseExpression();
+					eat(')');
+				} else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
+					while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+					x = Double.parseDouble(str.substring(startPos, this.pos));
+				} else if (ch >= 'a' && ch <= 'z') { // functions
+					while (ch >= 'a' && ch <= 'z') nextChar();
+					String func = str.substring(startPos, this.pos);
+					x = parseFactor();
+					x = switch (func) {
+						case "sqrt" -> Math.sqrt(x);
+						case "sin" -> Math.sin(Math.toRadians(x));
+						case "cos" -> Math.cos(Math.toRadians(x));
+						case "tan" -> Math.tan(Math.toRadians(x));
+						default -> throw new RuntimeException("Unknown function: " + func);
+					};
+				} else {
+					throw new RuntimeException("Unexpected: " + (char)ch);
+				}
 
-	            double x;
-	            int startPos = this.pos;
-	            if (eat('(')) { // parentheses
-	                x = parseExpression();
-	                eat(')');
-	            } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
-	                while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
-	                x = Double.parseDouble(str.substring(startPos, this.pos));
-	            } else if (ch >= 'a' && ch <= 'z') { // functions
-	                while (ch >= 'a' && ch <= 'z') nextChar();
-	                String func = str.substring(startPos, this.pos);
-	                x = parseFactor();
-	                if (func.equals("sqrt")) x = Math.sqrt(x);
-	                else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
-	                else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
-	                else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
-	                else throw new RuntimeException("Unknown function: " + func);
-	            } else {
-	                throw new RuntimeException("Unexpected: " + (char)ch);
-	            }
+				if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
 
-	            if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
-
-	            return x;
-	        }
-	    }.parse();
+				return x;
+			}
+		}.parse();
 	}
 }
