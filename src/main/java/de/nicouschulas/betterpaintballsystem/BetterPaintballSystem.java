242bd3b077a9d27e54e2b5f85512436e0c2443d6
Nicouschulas <156347186+Nicouschulas@users.noreply.github.com>
2026-07-16 17:15:19 +0200

    refactor(BetterPaintballSystem): replace printStackTrace with robust logging in saveMessages

--- src/main/java/de/nicouschulas/betterpaintballsystem/BetterPaintballSystem.java
+++ src/main/java/de/nicouschulas/betterpaintballsystem/BetterPaintballSystem.java
@@ -508,8 +508,8 @@ public class BetterPaintballSystem extends JavaPlugin {
 			try {
 				messages.save(messagesFile);
 			}catch (IOException e) {
-				 e.printStackTrace();
-		 	}
+				getLogger().log(java.util.logging.Level.SEVERE, "Could not save messages.yml!", e);
+			}
 		}
 		  
 		public FileConfiguration getMessages() {
